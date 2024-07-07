343e2716b66b99fb20a87c684f8a8741ca91765b
Akshat Mishra <akshatmishra317@gmail.com>
2024-07-07 17:51:10 +0530

    updated backend api routes

--- notes-backend/src/app.module.ts
+++ notes-backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'secret', // replace with your own password
+      username: 'postgres', 
+      entities: [User],
+      database: 'notesdb', // make new table in your postgres user with command "CREATE DATABASE notesdb;"
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
